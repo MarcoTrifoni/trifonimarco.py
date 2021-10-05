ee22d2d934b1ec2e96fbb8a367051bd313899c49
MarcoTrifoni <74866701+MarcoTrifoni@users.noreply.github.com>
2021-10-06 01:37:01 +0200

    Add files via upload

---
+++ calcolomb.py
@@ -0,0 +1,9 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #calcomb.py
+>>> import random
+>>> class k:
+	def __init__ (k=0, k>0, k<=0).
+	
+SyntaxError: invalid syntax
+>>> def __init
